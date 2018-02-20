a07c0f285d890be2c69997756c2eecb4ea938807
Alonso Avotz <alonso@avotz.com>
2018-02-20 16:29:11 -0600

    se optimizaron imagenes

--- wp-content/themes/crprivate/functions.php
+++ wp-content/themes/crprivate/functions.php
@@ -120,9 +120,9 @@ add_action( 'widgets_init', 'crprivate_widgets_init' );
 function crprivate_scripts() {
 	wp_enqueue_style( 'crprivate-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'crprivate-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	//wp_enqueue_script( 'crprivate-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'crprivate-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	//wp_enqueue_script( 'crprivate-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
