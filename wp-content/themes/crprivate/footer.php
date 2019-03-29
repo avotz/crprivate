829a3c7b976ff1c06a80d760b01afd7db20c78b0
Alonso <alonso@avotz.com>
2019-03-29 09:45:42 -0600

    se cambio correo en el footer

--- wp-content/themes/crprivate/footer.php
+++ wp-content/themes/crprivate/footer.php
@@ -11,59 +11,60 @@
 
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<footer class="footer">
-		<div class="inner">
-			<div class="footer-container">
-				<div class="footer-item">
-					<div class="copy">
-						CR Private Tours &copy; 2018. <a href="#"><i class="icon-avotz"></i></a>
-					</div>
-				</div>
-				<div class="footer-item">
-					<div class="contact">
-						<h3>Contact Us</h3>
-						<ul>
-							<li>Information: (506)8826-5425</li>
-							<li><a href="mailto::info@costaricaprivate.net">info@costaricaprivate.net</a> </li>
-							
-						</ul>
-						
+<footer class="footer">
+    <div class="inner">
+        <div class="footer-container">
+            <div class="footer-item">
+                <div class="copy">
+                    CR Private Tours &copy; 2018. <a href="#"><i class="icon-avotz"></i></a>
+                </div>
+            </div>
+            <div class="footer-item">
+                <div class="contact">
+                    <h3>Contact Us</h3>
+                    <ul>
+                        <li>Information: (506)8826-5425</li>
+                        <li><a href="mailto::info@costaricaprivatetours.net">info@costaricaprivatetours.net</a> </li>
 
-					</div>
-				</div>
-				<div class="footer-item">
-					<h3>Get in Touch</h3>
-					<div class="social">
-						<a href="https://www.facebook.com/Costa-Rica-Private-Tours-149812958422247/
+                    </ul>
+
+
+                </div>
+            </div>
+            <div class="footer-item">
+                <h3>Get in Touch</h3>
+                <div class="social">
+                    <a href="https://www.facebook.com/Costa-Rica-Private-Tours-149812958422247/
 " class="social-item" target="_blank"><i class="icon-facebook"></i></a>
-						<a href="https://www.tripadvisor.com.mx/Attraction_Review-g309240-d2149604-Reviews-Costa_Rica_Private_Tours-Liberia_Province_of_Guanacaste.html" class="social-item" target="_blank"><i class="fa fa-tripadvisor"></i></a>
-					</div>
-				</div>
-			</div>
-			
-			
-			
-			
-		</div>
-		
-	</footer>
-	
+                    <a href="https://www.tripadvisor.com.mx/Attraction_Review-g309240-d2149604-Reviews-Costa_Rica_Private_Tours-Liberia_Province_of_Guanacaste.html" class="social-item" target="_blank"><i class="fa fa-tripadvisor"></i></a>
+                </div>
+            </div>
+        </div>
+
+
+
+
+    </div>
+
+</footer>
+
 </div><!-- #page -->
 
-   <div id="tour-popup" class="request-popup white-popup mfp-hide mfp-with-anim">
-		<?php echo do_shortcode('[contact-form-7 id="183" title="Book Tour"]');
-	?>               
-	    
-	</div>
-	 <div id="shuttle-popup" class="request-popup white-popup mfp-hide mfp-with-anim">
-		<?php echo do_shortcode('[contact-form-7 id="184" title="Book Shuttle"]');
-	?>               
-	    
-	</div>
+<div id="tour-popup" class="request-popup white-popup mfp-hide mfp-with-anim">
+    <?php echo do_shortcode('[contact-form-7 id="183" title="Book Tour"]');
+	?>
+
+</div>
+<div id="shuttle-popup" class="request-popup white-popup mfp-hide mfp-with-anim">
+    <?php echo do_shortcode('[contact-form-7 id="184" title="Book Shuttle"]');
+	?>
+
+</div>
 
 <?php wp_footer(); ?>
 
 </body>
-</html>
+
+</html> 
